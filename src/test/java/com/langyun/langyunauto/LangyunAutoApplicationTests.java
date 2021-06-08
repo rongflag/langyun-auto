78fd8cbed41de6befe4ff320c20d0a54cc9738fc
langyun <worongbo@163.com>
2021-06-09 00:24:24 +0800

    新增: 初始化

---
+++ src/test/java/com/langyun/langyunauto/LangyunAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langyun.langyunauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangyunAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
